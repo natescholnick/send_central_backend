56fa36eb2fc5aab46750905951aaf557715add67
Nathaniel Scholnick <nate.scholnick@gmail.com>
2019-12-12 08:23:01 -0500

    misc

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = "this-is-a-secret"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
-    # 'postgresql://postgres:Moxysuq0@localhost:5432/sendcentral'
 
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USERNAME = 'sendcentralteam@gmail.com'
